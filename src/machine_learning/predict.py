5b03e882a62db25282082e296eb921384f2838e5
Kyra Erckert <kyra.erckert@tum.de>
2024-02-22 11:40:02 +0100

    Add model loading logic to run_predictions()

--- src/machine_learning/predict.py
+++ src/machine_learning/predict.py
@@ -1,6 +1,19 @@
 from machine_learning.Dataset import BindingResidueDataset
+from machine_learning.ModelManager import load_classifier_torch
+from setup.configProcessor import get_weight_dir
+
+import os
 
 
 def run_prediction():
     dataset = BindingResidueDataset()
+    predictions = {}
+
+    models = os.listdir(get_weight_dir())
+    for model in models:
+        model_path = os.path.join(get_weight_dir(), model)
+        pretrained_model = load_classifier_torch(model_path)
+
+        print(pretrained_model)
     return None
+
