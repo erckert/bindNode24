0fe5f89ebc40c158db225a8c55a028f49fc9a685
Kyra Erckert <kyra.erckert@tum.de>
2024-02-22 11:38:30 +0100

    Update Model manager to use state_dict instead of normal torch.load functionality. setup GCNConv model according to config

--- src/machine_learning/ModelManager.py
+++ src/machine_learning/ModelManager.py
@@ -1,9 +1,13 @@
 import torch
+from setup.configProcessor import select_model_type_from_config, get_in_channels, get_feature_channels, get_dropouts, \
+    get_out_channels
+from misc.enums import ModelType
+from machine_learning.Models import GCNConvModel, SAGEConvModel, SAGEConvMLPModel, SAGEConvGATMLPModel
 
 
 def save_classifier_torch(classifier, model_path):
     """Save pre-trained model"""
-    torch.save(classifier, model_path)
+    torch.save(classifier.state_dict(), model_path)
 
 
 def load_classifier_torch(model_path):
@@ -12,5 +16,21 @@ def load_classifier_torch(model_path):
         device = 'cuda:0'
     else:
         device = 'cpu'
-    classifier = torch.load(model_path, map_location=device)
+    model_type = select_model_type_from_config()
+    classifier = None
+    match model_type:
+        case ModelType.GCNCONV:
+            classifier = GCNConvModel(
+                in_channels=get_in_channels,
+                feature_channels=get_feature_channels(only_first_value=True),
+                out_channels=get_out_channels,
+                dropout=get_dropouts(only_first_value=True)
+            )
+        case ModelType.SAGECONV:
+            classifier = SAGEConvModel()
+        case ModelType.SAGECONVMLP:
+            classifier = SAGEConvMLPModel()
+        case ModelType.SAGECONVGATMLP:
+            classifier = SAGEConvGATMLPModel()
+    classifier.load_state_dict(torch.load(model_path, map_location=device))
     return classifier
