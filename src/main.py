a90f481909175d6b7cf9834e54463a6f60ab9902
Kyra Erckert <kyra.erckert@tum.de>
2024-02-26 10:28:54 +0100

    Include seeding in main

--- src/main.py
+++ src/main.py
@@ -3,9 +3,11 @@ from machine_learning.predict import run_prediction
 from machine_learning.train import run_optimization, run_training
 
 from setup.configProcessor import validate_config, select_mode_from_config
+from setup.generalSetup import seed_all
 
 if __name__ == "__main__":
     print('I am running')
+    seed_all(seed=42)
     validate_config()
     mode = select_mode_from_config()
     print(f'Selected mode is {mode.name}')
