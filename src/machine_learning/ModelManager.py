73d33fdf16901ee702b968d80a687fa1a99c267d
Kyra Erckert <kyra.erckert@tum.de>
2024-04-02 15:30:27 +0200

    Update model manager to load new models

--- src/machine_learning/ModelManager.py
+++ src/machine_learning/ModelManager.py
@@ -1,6 +1,7 @@
 import torch
+
 from setup.configProcessor import select_model_type_from_config, get_in_channels, get_feature_channels, get_dropouts, \
-    get_out_channels
+    get_out_channels, get_activation, get_dropouts_fcn
 from misc.enums import ModelType
 from machine_learning.Models import GCNConvModel, SAGEConvModel, SAGEConvMLPModel, SAGEConvGATMLPModel
 
@@ -17,11 +18,35 @@ def initialize_model_with_config_params():
                 dropout=get_dropouts(only_first_value=True)
             )
         case ModelType.SAGECONV:
-            classifier = SAGEConvModel()
+            classifier = SAGEConvModel(
+                in_channels=get_in_channels(),
+                feature_channels=get_feature_channels(only_first_value=True),
+                out_channels=get_out_channels(),
+                dropout=get_dropouts(only_first_value=True),
+                activation=get_activation()
+            )
         case ModelType.SAGECONVMLP:
-            classifier = SAGEConvMLPModel()
+            classifier = SAGEConvMLPModel(
+                in_channels=get_in_channels(),
+                feature_channels=get_feature_channels(only_first_value=True),
+                additional_channels=20, #TODO: get nr of eg. DSSP features from config
+                out_channels=get_out_channels(),
+                dropout=get_dropouts(only_first_value=True),
+                activation=get_activation(),
+                heads=4, #TODO: Add heads to config?
+                dropout_fcn=get_dropouts_fcn(only_first_value=True)
+            )
         case ModelType.SAGECONVGATMLP:
-            classifier = SAGEConvGATMLPModel()
+            classifier = SAGEConvGATMLPModel(
+                in_channels=get_in_channels(),
+                feature_channels=get_feature_channels(only_first_value=True),
+                additional_channels=20, #TODO: get nr of eg. DSSP features from config
+                out_channels=get_out_channels(),
+                dropout=get_dropouts(only_first_value=True),
+                activation=get_activation(),
+                heads=4, #TODO: Add heads to config?
+                dropout_fcn=get_dropouts_fcn(only_first_value=True)
+            )
     return classifier
 
 
